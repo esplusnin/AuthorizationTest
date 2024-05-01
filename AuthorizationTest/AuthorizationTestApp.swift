9f76d255a2e1044beb23d7bc140a5349d968ca4f
esplusnin <esplusnin94@yandex.ru>
2024-05-01 12:08:14 +0400

    Initial Commit

---
+++ AuthorizationTest/AuthorizationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationTestApp.swift
+//  AuthorizationTest
+//
+//  Created by Евгений on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
